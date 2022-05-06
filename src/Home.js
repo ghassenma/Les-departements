0bc7ba3c5c91a58bfbbc4e7a7efed2d9cd0cb477
ghassenma <104145253+ghassenma@users.noreply.github.com>
2022-05-06 10:56:43 +0200

    App | add departement

--- src/Home.js
+++ src/Home.js
@@ -8,6 +8,7 @@ class Home extends React.Component {
       <Box sx={{ display: "flex", flexDirection: "column" }}>
         <Link to="/shopping-list">Shopping list</Link>
         <Link to="/Connection">Connexion</Link>
+        <Link to="/Departementliste">Connexion</Link>
       </Box>
     );
   }
